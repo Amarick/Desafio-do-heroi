396570dcd6d9d38d265e2dfd1bcd8860d03a10e7
Victor Amaro <victorflamaro@gmail.com>
2024-02-19 17:00:57 -0300

    desafio dio

---
+++ index.js
@@ -0,0 +1,23 @@
+let nomeHeroi = "Amarick_Sensei"
+let pontosXP = 10000
+let nivelHeroi;
+
+if(pontosXP <= 1000){
+    nivelHeroi = "Ferro"
+}else if(pontosXP > 1000 && pontosXP <= 2000){
+    nivelHeroi =  "Bronze"
+}else if(pontosXP > 2000 && pontosXP <= 5000){
+    nivelHeroi = "Prata"
+}else if(pontosXP > 5000 && pontosXP <= 7000){
+    nivelHeroi = "Ouro"
+}else if(pontosXP > 7000 && pontosXP <= 8000){
+    nivelHeroi = "Platina"
+}else if(pontosXP > 8000 && pontosXP <= 9000){
+    nivelHeroi = "Ascendente"
+}else if(pontosXP > 9000 && pontosXP <= 10000){
+    nivelHeroi = "Imortal"
+}else{
+    nivelHeroi = "Radiante"
+}
+
+console.log("O Herói " + nomeHeroi + " está no nível " + nivelHeroi)
